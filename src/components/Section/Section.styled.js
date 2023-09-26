9852ed045923db3d2016f5f5d809bc288781f390
andrhwl <120339957+andrhwl@users.noreply.github.com>
2023-09-27 00:52:03 +0300

    add

--- src/components/Section/Section.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Sectionmain = styled.section`
-  font-size: 30px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
--- src/components/Statistics/Statistics.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Statlist = styled.li`
-  font-size: 22px;
-  margin-bottom: 5px;
-`;
